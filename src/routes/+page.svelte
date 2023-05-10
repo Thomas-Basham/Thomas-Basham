51f3999de4a088b2bf07beeb823edf1d5330c94f
Thomas Basham <Bashamtg@gmail.com>
2023-05-10 10:14:32 -0700

    updated typo

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -161,13 +161,13 @@
 					<h2 class="heading">About Me</h2>
 					<div class="content">
 						<p>
-							Hello, I am a software developer specializing in developing full stack dynamic web
-							applications with Python and JavaScript. I pursued my education in computer science at
-							Code Fellows and have been honing my skills ever since. Prior to my career in tech, I
-							worked as a carpenter, developing skills that have proven to be invaluable in my
-							current profession, such as attention to detail and problem-solving. Additionally, I
-							am a US army veteran who served from 2011-2015. I am based in Renton, WA and eager to
-							continue growing my knowledge and contributing to the industry.
+							Hello, I am a software developer making data look beautiful with Python and
+							JavaScript. I pursued my education in computer science at Code Fellows and have been
+							honing my skills ever since. Prior to my career in tech, I worked as a carpenter,
+							developing skills that have proven to be invaluable in my current profession, such as
+							attention to detail and problem-solving. Additionally, I am a US army veteran who
+							served from 2011-2015. I am based in Renton, WA and eager to continue growing my
+							knowledge and contributing to the industry.
 						</p>
 					</div>
 					<!--//content-->
